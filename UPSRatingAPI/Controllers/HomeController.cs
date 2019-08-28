5c80fc1d430fd3c2ac10869b2f885332ea1010fb
swonb <swonbinca@gmail.com>
2019-08-27 23:46:12 -0400

    Added Service Name
    changed service code to name (description)

--- UPSRatingAPI/Controllers/HomeController.cs
+++ UPSRatingAPI/Controllers/HomeController.cs
@@ -28,87 +28,101 @@ namespace UPSRatingAPI.Controllers
             //upssSvcAccessToken.AccessLicenseNumber = "3D6A1DD5F39023B5";
             //upss.ServiceAccessToken = upssSvcAccessToken;
 
-                UPSSecurity upss = new UPSSecurity();
+            UPSSecurity upss = new UPSSecurity();
 
-                UPSSecurityServiceAccessToken upssSvcAccessToken = new UPSSecurityServiceAccessToken();
-                upssSvcAccessToken.AccessLicenseNumber = accessToken;
-                upss.ServiceAccessToken = upssSvcAccessToken;
+            UPSSecurityServiceAccessToken upssSvcAccessToken = new UPSSecurityServiceAccessToken();
+            upssSvcAccessToken.AccessLicenseNumber = accessToken;
+            upss.ServiceAccessToken = upssSvcAccessToken;
 
-                UPSSecurityUsernameToken upssUsrNameToken = new UPSSecurityUsernameToken();
-                upssUsrNameToken.Username = username;
-                upssUsrNameToken.Password = password;
-                upss.UsernameToken = upssUsrNameToken;
+            UPSSecurityUsernameToken upssUsrNameToken = new UPSSecurityUsernameToken();
+            upssUsrNameToken.Username = username;
+            upssUsrNameToken.Password = password;
+            upss.UsernameToken = upssUsrNameToken;
 
-                RateRequest rateRequest = new RateRequest();
+            RateRequest rateRequest = new RateRequest();
 
-                RequestType request = new RequestType();
-                //String[] requestOption = { "Rate" };
-                String[] requestOption = { "Shop" };
-                request.RequestOption = requestOption;
-                rateRequest.Request = request;
+            RequestType request = new RequestType();
+            //String[] requestOption = { "Rate" };
+            String[] requestOption = { "Shop" };
+            request.RequestOption = requestOption;
+            rateRequest.Request = request;
 
-                ShipmentType shipment = new ShipmentType();
+            ShipmentType shipment = new ShipmentType();
 
-                ShipperType shipper = new ShipperType();
-                AddressType shipperAddress = new AddressType();
-                shipperAddress.City = "Roswell";
-                shipperAddress.PostalCode = "30076";
-                shipperAddress.StateProvinceCode = "GA";
-                shipperAddress.CountryCode = "US";
-                shipper.Address = shipperAddress;
-                shipment.Shipper = shipper;
+            ShipperType shipper = new ShipperType();
+            AddressType shipperAddress = new AddressType();
+            //shipperAddress.City = "Roswell";
+            //shipperAddress.PostalCode = "30076";
+            //shipperAddress.StateProvinceCode = "GA";
+            //shipperAddress.CountryCode = "US";
+            shipperAddress.City = "Toronto";
+            shipperAddress.PostalCode = "M1P4P5";
+            shipperAddress.CountryCode = "CA";
+            shipper.Address = shipperAddress;
+            shipment.Shipper = shipper;
 
-                ShipFromType shipFrom = new ShipFromType();
-                ShipAddressType shipFromAddress = new ShipAddressType();
-                shipFromAddress.City = "Roswell";
-                shipFromAddress.PostalCode = "30076";
-                shipFromAddress.StateProvinceCode = "GA";
-                shipFromAddress.CountryCode = "US";
-                shipFrom.Address = shipFromAddress;
-                shipment.ShipFrom = shipFrom;
+            ShipFromType shipFrom = new ShipFromType();
+            ShipAddressType shipFromAddress = new ShipAddressType();
+            //shipFromAddress.City = "Roswell";
+            //shipFromAddress.PostalCode = "30076";
+            //shipFromAddress.StateProvinceCode = "GA";
+            //shipFromAddress.CountryCode = "US";
+            shipFromAddress.City = "Toronto";
+            shipFromAddress.PostalCode = "M1P4P5";
+            shipFromAddress.CountryCode = "CA";
+            shipFrom.Address = shipFromAddress;
+            shipment.ShipFrom = shipFrom;
 
-                ShipToType shipTo = new ShipToType();
-                ShipToAddressType shipToAddress = new ShipToAddressType();
-                shipToAddress.City = "Plam Springs";
-                shipToAddress.PostalCode = "92262";
-                shipToAddress.StateProvinceCode = "CA";
-                shipToAddress.CountryCode = "US";
-                shipTo.Address = shipToAddress;
-                shipment.ShipTo = shipTo;
+            ShipToType shipTo = new ShipToType();
+            ShipToAddressType shipToAddress = new ShipToAddressType();
+            //shipToAddress.City = "Plam Springs";
+            //shipToAddress.PostalCode = "92262";
+            //shipToAddress.StateProvinceCode = "CA";
+            //shipToAddress.CountryCode = "US";
+            shipToAddress.City = "Toronto";
+            shipToAddress.PostalCode = "M1P4P5";
+            shipToAddress.CountryCode = "CA";
+            shipTo.Address = shipToAddress;
+            shipment.ShipTo = shipTo;
 
-                CodeDescriptionType service = new CodeDescriptionType();
-                //Below code uses dummy date for reference. Please udpate as required.
-                service.Code = "02";
-                shipment.Service = service;
+            CodeDescriptionType service = new CodeDescriptionType();
+            //Below code uses dummy date for reference. Please udpate as required.
+            service.Code = "02";
+            shipment.Service = service;
 
-                PackageType package = new PackageType();
-                PackageWeightType packageWeight = new PackageWeightType();
-                packageWeight.Weight = "125";
-                CodeDescriptionType uom = new CodeDescriptionType();
-                uom.Code = "LBS";
-                uom.Description = "pounds";
-                packageWeight.UnitOfMeasurement = uom;
-                package.PackageWeight = packageWeight;
-                CodeDescriptionType packType = new CodeDescriptionType();
-                packType.Code = "02";
-                package.PackagingType = packType;
-                PackageType[] pkgArray = { package };
-                shipment.Package = pkgArray;
+            PackageType package = new PackageType();
+            PackageWeightType packageWeight = new PackageWeightType();
+            packageWeight.Weight = "10";
+            CodeDescriptionType uom = new CodeDescriptionType();
+            uom.Code = "LBS";
+            uom.Description = "pounds";
+            packageWeight.UnitOfMeasurement = uom;
+            package.PackageWeight = packageWeight;
+            CodeDescriptionType packType = new CodeDescriptionType();
+            packType.Code = "02";
+            package.PackagingType = packType;
+            PackageType[] pkgArray = { package };
+            shipment.Package = pkgArray;
 
-                rateRequest.Shipment = shipment;
+            rateRequest.Shipment = shipment;
 
-                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11; //This line will ensure the latest security protocol for consuming the web service call.
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11; //This line will ensure the latest security protocol for consuming the web service call.
 
-                RatePortTypeClient client = new RatePortTypeClient();
-                RateResponse rateResponse = client.ProcessRate(upss, rateRequest);
+            RatePortTypeClient client = new RatePortTypeClient();
+            RateResponse rateResponse = client.ProcessRate(upss, rateRequest);
 
-            RatePackage model = new RatePackage()
+
+            foreach (var item in rateResponse.RatedShipment)
+            {
+                item.Service.Description = ServiceCode.GetName(item.Service.Code);
+            }
+
+                RatePackage model = new RatePackage()
             {
                 Response = rateResponse
             };
 
-
-            return View(model);
+                return View(model);
         }
 
         public ActionResult About()
---
+++ UPSRatingAPI/Models/ServiceCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UPSRatingAPI.Models
+{
+    public static class ServiceCode
+    {
+        //public int number { get; set; }
+        //public string name { get; set; }
+
+        static Dictionary<string, string> codeName = new Dictionary<string, string>();
+
+        static ServiceCode()
+        {
+            codeName.Add("02", "UPS Expedited");
+            codeName.Add("13", "UPS Express Saver");
+            codeName.Add("12", "UPS 3 Day Select");
+            codeName.Add("70", "UPS Access Point Economy");
+            codeName.Add("01", "UPS Express");
+            codeName.Add("14", "UPS Express Early");
+            codeName.Add("65", "UPS Express Saver");
+            codeName.Add("11", "UPS Standard");
+            codeName.Add("08", "UPS Worldwide Expedited");
+            codeName.Add("07", "UPS Worldwide Express");
+            codeName.Add("54", "UPS Worldwide Express Plus");
+        }
+
+        public static string GetName(string code)
+        {
+            string result = "";
+
+            if (codeName.ContainsKey(code))
+            {
+                result = codeName[code];
+            }
+
+            return result;
+        }
+    }
+}
